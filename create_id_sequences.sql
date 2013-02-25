e17304fedc932a4a4aeb42e7488fee1b9471c6eb
Martin Zottmann <github@eiscp.de>
2013-02-25 15:13:29 +0100

    Better substituion variables and fix whitespaces

--- create_id_sequences.sql
+++ create_id_sequences.sql
@@ -1,11 +1,11 @@
 SELECT
     'CREATE SEQUENCE ' || USER || '.' || SUBSTR('SQ_ID_' || T.TABLE_NAME, 1, 30) || CHR(13) || CHR(10) ||
-     ' START WITH 1 ' || CHR(13) || CHR(10) ||
-     ' MAXVALUE 999999999999999999999999999' || CHR(13) || CHR(10) ||
-     ' MINVALUE 1' || CHR(13) || CHR(10) ||
-     ' NOCYCLE' || CHR(13) || CHR(10) ||
-     ' NOCACHE' || CHR(13) || CHR(10) ||
-     ' NOORDER;' || CHR(13) || CHR(10) AS COMMANDS
+    '    START WITH 1 ' || CHR(13) || CHR(10) ||
+    '    MAXVALUE 999999999999999999999999999' || CHR(13) || CHR(10) ||
+    '    MINVALUE 1' || CHR(13) || CHR(10) ||
+    '    NOCYCLE' || CHR(13) || CHR(10) ||
+    '    NOCACHE' || CHR(13) || CHR(10) ||
+    '    NOORDER;' || CHR(13) || CHR(10) AS COMMANDS
 FROM USER_TABLES T
 WHERE
     EXISTS (
